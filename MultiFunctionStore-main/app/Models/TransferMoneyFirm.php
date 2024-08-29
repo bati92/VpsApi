5874accd2bcc950103516b31e57b76fb66308998
batoul srour <bito.eng92@gmail.com>
2024-08-28 21:55:30 -0700

    E-Bank

---
+++ MultiFunctionStore-main/app/Models/TransferMoneyFirm.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class TransferMoneyFirm extends Model
+{
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'iban',
+        'account_name',
+    ];
+    
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class);
+    }
+    
+}
